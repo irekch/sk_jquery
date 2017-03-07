7b22dfefb0a7b77c818964ad92014cc75b673943
Ireneusz Chojnacki <irek40@interia.pl>
2017-03-07 09:20:49 +0000

    wstep do jQuery

---
+++ JQ_L01_05_funkcjaJQuery/script.js
@@ -0,0 +1,29 @@
+// JavaScript File
+
+//to ponizej zapobiega sygnalizowaniu bledu ze stosowaniem $ w Cloud9
+/* global $ */
+
+/*
+Znak "$" oznacza funkcje o nazwie "jQuery". tak wiec zamiast "$" moglibysmy zapisac
+"jQuery" i efekt bedzie ten sam
+*/
+
+/*
+//jQuery w zapisie nieco dluzszym
+$(document).ready(function(){
+    alert("Kurs Javascript with JQuery");
+});
+*/
+
+/*
+//to samo co powyzej, ale w skroconym zapisie
+$(function(){
+    alert("Kurs JS with JQuery krócej");
+})
+*/
+
+//to samo, co powyzej ale ze zdefiniowaniem funkcji Javascript
+function pokazOkno(){
+    alert("Okienko z jQuery");
+}
+$(pokazOkno);
